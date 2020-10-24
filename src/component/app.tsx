1478818761f2824489269022b0a0c25742d9905f
MunsMan <hendrik.munske@me.com>
2020-10-24 20:23:12 +0200

    Running the first Preact App

---
+++ src/component/app.tsx
@@ -0,0 +1,15 @@
+import { h, FunctionComponent } from 'preact';
+
+interface App {
+
+}
+
+const app: FunctionComponent<App> = () => {
+    return (
+        <div>
+            Hello from React
+        </div>
+    );
+};
+
+export default app;
---
+++ webpack.config.ts
@@ -0,0 +1,60 @@
+// @ts-ignore
+process.traceDeprecation = true;
+
+import webpack from 'webpack';
+import { resolve, join } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const context = __dirname;
+
+interface WebpackEnvironment {
+    NODE_ENV: string;
+}
+
+module.exports = (env: WebpackEnvironment, argv: { mode: string, port?: number }) => {
+    const port =  argv.port || 3000;
+    const config: webpack.Configuration = {
+        name: 'client',
+        target: 'web',
+        context,
+        entry: 
+            './src/index.tsx',
+        output: {
+            filename: 'bundle.js',
+            path: resolve(__dirname, 'build')
+        },
+        devtool: argv.mode === 'production' ? false : 'eval-source-map',
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                hash: true,
+                filename: 'index.html',
+                inject: 'body'
+            }),
+        ]
+    }
+
+    if (argv.mode === 'development') {
+        config.devServer = {
+            contentBase: join(__dirname, 'dist'),
+            compress: true,
+            port: port
+        }
+        const { HotModuleReplacementPlugin } = webpack;
+        config.plugins?.push(new HotModuleReplacementPlugin)
+    }
+
+    return config;
+}
